413e476bb1f349bccb52e83809f0bb68ecc29239
obidovbek <obidov.bekzod94@gmail.com>
2024-05-20 14:49:30 +0500

    Первоначальная фиксация

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secretKey', // Используйте свой секретный ключ
+      secretOrKey: 'secretKey',
     });
   }
 
