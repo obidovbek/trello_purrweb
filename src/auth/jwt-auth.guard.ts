a804a38112264c00b89e92a64454d7bac293e62f
obidovbek <obidov.bekzod94@gmail.com>
2024-05-20 14:49:00 +0500

    Первоначальная фиксация

--- src/auth/jwt-auth.guard.ts
+++ src/auth/jwt-auth.guard.ts
@@ -8,12 +8,10 @@ import { AuthGuard } from '@nestjs/passport';
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
   canActivate(context: ExecutionContext) {
-    // Добавьте любую пользовательскую логику активации здесь
     return super.canActivate(context);
   }
 
   handleRequest(err, user, info) {
-    // Вы можете выбросить ошибку или вернуть пользователя для привязки к request
     if (err || !user) {
       throw err || new UnauthorizedException();
     }
